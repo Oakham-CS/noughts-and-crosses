bd49c93d5d40fc269d0be1f5b0f6fa3dbc4435fb
BakerHarry <harrybaker@keeday.co.uk>
2020-03-24 10:09:07 +0000

    Finished Board Class
    Finished functions+methods:__init__, displayBoard, setGrid, getGrid, clearBoard

--- nac.py
+++ nac.py
@@ -7,14 +7,27 @@ class Board:
     # TODO HARRY
     # Please add in-line comments describing this class and it's properties/functions
 
-    def __init__(self, w, h):
-        self.width, self.height = w, h
+    def __init__(self):
+        self.width, self.height = 3, 3
+        self.grid = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
     def displayBoard(self):        
-        pass
+        print(self.grid[0], "|", self.grid[1], "|", self.grid[2])
+        print("---------")
+        print(self.grid[3], "|", self.grid[4], "|", self.grid[5])
+        print("---------")
+        print(self.grid[6], "|", self.grid[7], "|", self.grid[8])
+        print ("\n ============================================================================")
 
+
+    def setGrid(self, counter, index):
+        self.grid[index] = counter
+    def getGrid(self, index):
+        return(self.grid[index])
+        
     def clearBoard(self):
-        pass
+        #reset grid
+        self.grid = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
 class Game:
@@ -48,8 +61,9 @@ class Player:
 # some test code...
 
 if __name__ == "__main__":
-
-    board = Board(3, 3)
+    
+    board = Board()
+    board.displayBoard()
     susan = Player('Susan','X')
     bob = Player('Bob','O')
     game = Game(bob, susan, board) 
